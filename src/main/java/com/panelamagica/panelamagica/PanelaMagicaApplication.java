e5e61ef086467971c3e75f8f78aa4d3e19981bab
Lucas <lucas_b.xavier@hotmail.com>
2025-09-11 01:01:31 -0300

    innit commit

---
+++ src/main/java/com/panelamagica/panelamagica/PanelaMagicaApplication.java
@@ -0,0 +1,13 @@
+package com.panelamagica.panelamagica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelaMagicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanelaMagicaApplication.class, args);
+    }
+
+}
